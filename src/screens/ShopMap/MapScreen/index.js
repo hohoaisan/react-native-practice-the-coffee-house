c09b9d4b203ed1a51e6e360ab5aa7ab8d8433b2a
Hoai San <hohoaisan@gmail.com>
2020-12-24 02:33:33 +0700

    Detect location, move to current location if possible

--- src/screens/ShopMap/MapScreen/index.js
+++ src/screens/ShopMap/MapScreen/index.js
@@ -6,10 +6,15 @@ import CardSmall from '../../../components/CardSmall';
 import {firebase} from '../../../firebase';
 import DropDownPicker from 'react-native-dropdown-picker';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
+import {PermissionsAndroid} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+import Snackbar from 'react-native-snackbar';
 let listRef = React.createRef();
 
 const db = firebase.firestore();
+
+// Geolocation.setRNConfiguration(config);
+
 const MapScreen = ({navigation}) => {
   const map = useRef(null);
   const [region, setRegion] = useState({
@@ -21,7 +26,86 @@ const MapScreen = ({navigation}) => {
   const [markers, setMarkers] = useState([]);
   const [regions, setRegions] = useState([]);
   const [defaultDropdownValue, setDefaultDropdownValue] = useState(null);
-  const getLocationsOfSeletedRegion = (regionid) => {
+
+  const animateToRegion = ({
+    latitudeDelta,
+    longitudeDelta,
+    latitude,
+    longitude,
+  }) => {
+    map.current.animateToRegion({
+      latitudeDelta,
+      longitudeDelta,
+      latitude,
+      longitude,
+    });
+  };
+  const handleRegionSelectionChange = ({value, payloads}) => {
+    setRegion({
+      ...region,
+      ...payloads.coordinate,
+    });
+    getLocationsOfSeletedRegion(value, (markers) => setMarkers(markers));
+  };
+
+  const scrollListToIndex = (index) => {
+    listRef.current.scrollToIndex({index});
+  };
+  const snackBarShowMessage = (message, options) => {
+    Snackbar.show({
+      text: message,
+      duration: Snackbar.LENGTH_SHORT,
+      action: {
+        text: 'Bỏ qua',
+        textColor: 'green',
+        onPress: () => {
+          Snackbar.dismiss();
+        },
+      },
+      ...options,
+    });
+  };
+  const moveToUserLocation = async ({animated}) => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'The Coffee House',
+          message: 'The Coffee House want to access to your location ',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        Geolocation.getCurrentPosition(
+          ({coords}) => {
+            const {latitude, longitude} = coords;
+            if (animated) {
+              animateToRegion({...region, latitude, longitude});
+            } else {
+              setRegion({
+                ...region,
+                latitude,
+                longitude,
+              });
+            }
+          },
+          (err) => {
+            snackBarShowMessage('Bạn chưa bật định vị');
+          },
+          {
+            enableHighAccuracy: true,
+          },
+        );
+      } else {
+      }
+    } catch (err) {
+      snackBarShowMessage('Vui lòng cấp quyền cho ứng dụng truy cập vị trí');
+    }
+  };
+  const getLocationsOfSeletedRegion = (
+    regionid,
+    callbackSuccess,
+    callbackFailue,
+  ) => {
     const regionRef = db.collection('regions').doc(regionid);
     db.collection('shops')
       .where('region', '==', regionRef)
@@ -34,20 +118,19 @@ const MapScreen = ({navigation}) => {
             longitude: doc.data().coordinate.longitude,
           },
         }));
-        setMarkers(markers);
+        if (typeof callbackSuccess === 'function') {
+          callbackSuccess(markers);
+        }
+      })
+      .catch((err) => {
+        if (typeof callbackFailue === 'function') {
+          callbackFailue(err);
+        } else {
+          console.log(err);
+        }
       });
   };
-  const hanldeRegionChange = ({value, payloads}) => {
-    setRegion({
-      ...region,
-      ...payloads.coordinate,
-    });
-    getLocationsOfSeletedRegion(value);
-  };
-  const scrollListToIndex = (index) => {
-    listRef.current.scrollToIndex({index});
-  };
-  useEffect(() => {
+  const getRegions = (callbackSuccess, callbackFailue) => {
     db.collection('regions')
       .orderBy('order')
       .get()
@@ -64,12 +147,28 @@ const MapScreen = ({navigation}) => {
             },
           },
         }));
-        setRegions(regions);
-        // if (regions.length) {
-        //   setDefaultDropdownValue(regions[0].value);
-        //   hanldeRegionChange(regions[0]);
-        // }
+        if (typeof callbackSuccess === 'function') {
+          callbackSuccess(regions);
+        }
+      })
+      .catch((err) => {
+        if (typeof callbackFailue === 'function') {
+          callbackFailue(err);
+        } else {
+          console.warn(err);
+        }
       });
+  };
+  
+  useEffect(() => {
+    getRegions((regions) => {
+      console.log(regions);
+      setRegions(regions);
+      // if (regions.length) {
+      //   setDefaultDropdownValue(regions[0].value);
+      //   handleRegionSelectionChange(regions[0]);
+      // }
+    });
   }, []);
   console.log('map render');
   return (
@@ -78,7 +177,9 @@ const MapScreen = ({navigation}) => {
         ref={map}
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        region={region}>
+        region={region}
+        onMapReady={() => moveToUserLocation({animated: false})}
+        showsUserLocation={true}>
         {markers.length
           ? markers.map(({title, address, coordinate}, index) => {
               return (
@@ -88,8 +189,7 @@ const MapScreen = ({navigation}) => {
                   coordinate={{
                     longitude: coordinate.longitude ? coordinate.longitude : 0,
                     latitude: coordinate.latitude ? coordinate.latitude : 0,
-                  }}
-                >
+                  }}>
                   <IconButton
                     iconname="cafe-outline"
                     size={20}
@@ -109,7 +209,7 @@ const MapScreen = ({navigation}) => {
             itemStyle={{
               justifyContent: 'flex-start',
             }}
-            onChangeItem={hanldeRegionChange}
+            onChangeItem={handleRegionSelectionChange}
           />
         </View>
       </View>
@@ -120,7 +220,8 @@ const MapScreen = ({navigation}) => {
               iconname="location-outline"
               size={25}
               padding={20}
-              style={styles.locationPointer}></IconButton>
+              style={styles.locationPointer}
+              onPress={() => moveToUserLocation({animated: true})}></IconButton>
           </View>
         </View>
         <View>
@@ -132,7 +233,7 @@ const MapScreen = ({navigation}) => {
                   item={item}
                   key={index}
                   onPress={() =>
-                    map.current.animateToRegion({...region, ...item.coordinate})
+                    animateToRegion({...region, ...item.coordinate})
                   }></CardSmall>
               )}
               data={markers}
