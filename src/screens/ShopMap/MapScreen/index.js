6f75c43cb289c89fc259d442ab0e536003abfb77
Hoai San <hohoaisan@gmail.com>
2020-12-23 18:58:52 +0700

    Load region and location on region change

--- src/screens/ShopMap/MapScreen/index.js
+++ src/screens/ShopMap/MapScreen/index.js
@@ -4,30 +4,68 @@ import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import IconButton from '../../../components/electrons/IconButton';
 import CardSmall from '../../../components/CardSmall';
 import {firebase} from '../../../firebase';
-
+import DropDownPicker from 'react-native-dropdown-picker';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const db = firebase.firestore();
-
 const MapScreen = ({navigation}) => {
   const map = useRef(null);
   const [region, setRegion] = useState({
     latitude: 16.0697261,
     longitude: 108.2173665,
-    latitudeDelta: 0.015,
-    longitudeDelta: 0.0121,
+    latitudeDelta: 0.02,
+    longitudeDelta: 0.02,
   });
   const [markers, setMarkers] = useState([]);
-  useEffect(() => {
-    return db.collection('shops').onSnapshot((querySnapShot) => {
-      const marker = querySnapShot.docs.map((doc) => ({
-        ...doc.data(),
-        coordinate: {
-          latitude: doc.data().coordinate.latitude,
-          longitude: doc.data().coordinate.longitude,
-        },
-      }));
-      setMarkers(marker);
+  const [regions, setRegions] = useState([]);
+  const [defaultDropdownValue, setDefaultDropdownValue] = useState(null);
+  const getLocationsOfSeletedRegion = (regionid) => {
+    const regionRef = db.collection('regions').doc(regionid);
+    db.collection('shops')
+      .where('region', '==', regionRef)
+      .get()
+      .then((querySnapShot) => {
+        const markers = querySnapShot.docs.map((doc) => ({
+          ...doc.data(),
+          coordinate: {
+            latitude: doc.data().coordinate.latitude,
+            longitude: doc.data().coordinate.longitude,
+          },
+        }));
+        setMarkers(markers);
+      });
+  };
+  const hanldeRegionChange = ({value, payloads}) => {
+    setRegion({
+      ...region,
+      ...payloads.coordinate,
     });
+    getLocationsOfSeletedRegion(value);
+  };
+  useEffect(() => {
+    db.collection('regions')
+      .orderBy('order')
+      .get()
+      .then((querySnapshot) => {
+        const regions = querySnapshot.docs.map((region) => ({
+          label: region.data().title,
+          value: region.id,
+          payloads: {
+            ...region.data(),
+            id: region.id,
+            coordinate: {
+              latitude: region.data().coordinate.latitude,
+              longitude: region.data().coordinate.longitude,
+            },
+          },
+        }));
+        setRegions(regions);
+        // if (regions.length) {
+        //   setDefaultDropdownValue(regions[0].value);
+        //   hanldeRegionChange(regions[0]);
+        // }
+      });
   }, []);
+  console.log('map render');
   return (
     <View style={styles.container}>
       <MapView
@@ -40,7 +78,7 @@ const MapScreen = ({navigation}) => {
               return (
                 <Marker
                   key={index}
-                  onPress={(event) => console.log(event)}
+                  // onPress={(event) => console.log(event)}
                   coordinate={{
                     longitude: coordinate.longitude ? coordinate.longitude : 0,
                     latitude: coordinate.latitude ? coordinate.latitude : 0,
@@ -56,6 +94,22 @@ const MapScreen = ({navigation}) => {
             })
           : null}
       </MapView>
+      <View style={styles.regionSelection}>
+        <View>
+          <DropDownPicker
+            items={regions}
+            defaultValue={defaultDropdownValue}
+            placeholder="Chọn khu vực"
+            containerStyle={{height: 40}}
+            style={{backgroundColor: '#fafafa'}}
+            itemStyle={{
+              justifyContent: 'flex-start',
+            }}
+            dropDownStyle={{backgroundColor: '#fafafa'}}
+            onChangeItem={hanldeRegionChange}
+          />
+        </View>
+      </View>
       <View style={styles.listContainer}>
         {markers.length ? (
           <FlatList
@@ -93,6 +147,13 @@ const styles = StyleSheet.create({
   listContainer: {
     position: 'absolute',
     bottom: 0,
+    left: 0,
+    width: '100%',
+    padding: 10,
+  },
+  regionSelection: {
+    position: 'absolute',
+    top: 0,
     left: 0,
     width: '100%',
     padding: 10,
